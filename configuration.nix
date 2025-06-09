038a339cf9bdce8e4b1f1e798a7c4b18c7f3ee06
Melkorthewise <76819583+Melkorthewise@users.noreply.github.com>
2025-06-09 16:25:30 +0200

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,71 +1,169 @@
-{ config, pkgs, ... }:
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, inputs, ... }:
 
 {
-
   imports = [ ./hardware-configuration.nix ];
-  
-  programs.hyprland.enable = true;
-  programs.home-manager.enable = true;
 
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  programs.hyprland.enable = true;
+  programs.hyprland.xwayland.enable = true;
+
+  # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  i18n.defaultLocale = "en_GB.UTF-8";
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-  i18n.supportedLocales = [ "en_GB.UTF-8/UTF-8" "nl_NL.UTF-8/UTF-8" ];
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-  i18n.extraLocaleSettings = {
-    LC_NUMERIC		= "nl_NL.UTF-8";
-    LC_TIME		= "nl_NL.UTF-8";
-    LC_MONETARY		= "nl_NL.UTF-8";
-    LC_MEASUREMENT	= "nl_NL.UTF-8";
-    LC_ADDRESS		= "nl_NL.UTF-8";
-    LC_IDENTIFICATION	= "nl_NL.UTF-8";
-    LC_NAME		= "nl_NL.UTF-8";
-    LC_TELEPHONE	= "nl_NL.UTF-8";
-    LC_PAPER		= "nl_NL.UTF-8";
-    LC_COLLATE		= "nl_NL.UTF-8";
-    LC_MESSAGES		= "en_GB.UTF-8";
-  };
+  # Enable networking
+  networking.networkmanager.enable = true;
 
+  # Set your time zone.
   time.timeZone = "Europe/Amsterdam";
 
-  programs.hyprland.xwayland.enable = true;
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_GB.UTF-8";
 
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "nl_NL.UTF-8";
+    LC_IDENTIFICATION = "nl_NL.UTF-8";
+    LC_MEASUREMENT = "nl_NL.UTF-8";
+    LC_MONETARY = "nl_NL.UTF-8";
+    LC_NAME = "nl_NL.UTF-8";
+    LC_NUMERIC = "nl_NL.UTF-8";
+    LC_PAPER = "nl_NL.UTF-8";
+    LC_TELEPHONE = "nl_NL.UTF-8";
+    LC_TIME = "nl_NL.UTF-8";
+  };
+
+  # Enable the X11 windowing system.
   services.xserver.enable = true;
+
+  # Enable the GNOME Desktop Environment.
+  # services.xserver.displayManager.gdm.enable = true;
+  # services.xserver.desktopManager.gnome.enable = true;
+
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.displayManager.gdm.wayland = true;
 
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "euro";
+  };
+
+  environment.variables = {
+    XCURSOR_THEME = "Bibata-Modern-Ice";
+    XCURSOR_SIZE = "24";
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.tdmunnik = {
     isNormalUser = true;
     description = "Thomas de Munnik";
-    extraGroups = [ "networkmanager" "wheel" ];
+    extraGroups = [ "networkmanager" "wheel" "dialout" "docker" ];
+    packages = with pkgs; [
+    #  thunderbird
+    ];
   };
 
+  # Enable automatic login for the user.
+  services.displayManager.autoLogin.enable = false;
+  services.displayManager.autoLogin.user = "tdmunnik";
+
+  # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
+  systemd.services."getty@tty1".enable = false;
+  systemd.services."autovt@tty1".enable = false;
+
+  # Install firefox.
+  programs.firefox.enable = true;
+
+  # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
   environment.systemPackages = with pkgs; [
     neovim
+    zed-editor
+    
     kitty
-    wofi
     firefox
+    git
+    wget
+    unzip
+    imagemagick
+    luarocks
+    lsof
+    libnotify
+    bibata-cursors
+    parted
+
+    wofi
     waybar
     hyprpaper
     hyprlock
-    hyprsunset
-    git
+    hyprshot
+    mako
+
     spotify
     obsidian
     discord
-    unzip
-    playerctl
-    nautilus
-    pipewire
     steam
-    mako
-    pavucontrol
+    prismlauncher
+    libreoffice
+
+    python3
+    lua5_1
     go
-    home-manager
+    arduino-cli
+    arduino-ide
+    docker
+    docker-compose
+    lazydocker
+    nodejs
+    nodePackages.npm
+    gcc
+
+    playerctl
+    pavucontrol
+    blueman
+    nautilus
+    networkmanager
+    networkmanagerapplet
+    viu
+
+    inputs.zen-browser.packages.${pkgs.system}.default
   ];
 
   hardware.bluetooth.enable = true;
@@ -73,23 +171,42 @@
 
   services.blueman.enable = true;
 
-  programs.ssh.startAgent = true;
-
-  programs.neovim = {
-    enable = true;
-    viAlias = true;
-    vimAlias = true;
-    # optionally: withNodeJs = true; # if you want extra plugins working
-  };
-
   fonts.packages = with pkgs; [
     (nerdfonts.override { fonts = [ "FiraCode" "JetBrainsMono" "Iosevka" ]; })
   ];
 
-  nix.settings = {
-    experimental-features = [ "nix-command" "flakes" ];
-  };
+  virtualisation.docker.enable = true;
+  virtualisation.docker.enableOnBoot = true;
 
-  system.stateVersion = "24.11";
+  environment.localBinInPath = true;
 
-} 
+  services.postgresql.enable = false;
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.11"; # Did you read the comment?
+
+}
